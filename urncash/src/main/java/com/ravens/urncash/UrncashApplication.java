1f46ae2a0b70dbea3fbec2c530839773d96d8057
Bappaditya Dutta <bappadityadutta2019@gmail.com>
2022-01-26 08:20:35 +0530

    UrnCash Project Settings

---
+++ urncash/src/main/java/com/ravens/urncash/UrncashApplication.java
@@ -0,0 +1,13 @@
+package com.ravens.urncash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrncashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrncashApplication.class, args);
+	}
+
+}
---
+++ urncash/src/test/java/com/ravens/urncash/UrncashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravens.urncash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrncashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
