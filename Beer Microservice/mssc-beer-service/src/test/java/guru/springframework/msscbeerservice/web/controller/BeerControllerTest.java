985e7d6064e236eb7b7e8ced65de8301ff2190d3
Bappaditya Dutta <bappadityadutta2019@gmail.com>
2022-02-25 10:30:07 +0530

    Configuring Junit 5 and basic controller Test

---
+++ Beer Microservice/mssc-beer-service/src/test/java/guru/springframework/msscbeerservice/web/controller/BeerControllerTest.java
@@ -0,0 +1,42 @@
+package guru.springframework.msscbeerservice.web.controller;
+
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import java.util.UUID;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import guru.springframework.msscbeerservice.services.BeerService;
+
+
+@WebMvcTest(BeerController.class)
+class BeerControllerTest {
+	
+	@Autowired
+    MockMvc mockMvc;
+	
+	@Autowired
+    ObjectMapper objectMapper;
+	
+	@MockBean
+    BeerService beerService;
+
+	@Test
+	void getBeerById() throws Exception {
+		
+		//given(beerService.getById(any(), anyBoolean())).
+		mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk());
+	}
+
+}
